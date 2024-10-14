d3a1933e7b9b3c564061a14da5f744ed9fa51002
banditachalise <chalisba@mail.uc.edu>
2024-10-14 11:58:17 -0400

    Add files via upload
    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+        public class Main {
+            public static void main(String[] args) {
+                for (int i = 0; i <= 30; i++) {
+                    System.out.print(i + " ");
+                    //System.out.println("the numbers are even");
+                }
+            }
+        }
